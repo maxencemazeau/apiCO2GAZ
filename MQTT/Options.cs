55c54aa91c0ece18a46ade8b8c41ce9cbd06cc30
maxencemazeau <89794067+maxencemazeau@users.noreply.github.com>
2023-04-20 10:54:53 -0400

    L'utilisateur peut choisir le topic auqul il subscribe avec un input

--- MQTT/Options.cs
+++ MQTT/Options.cs
@@ -22,7 +22,9 @@ namespace MQTT
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = false)]
     public class Options : Activity
     {
-        private ListView _historiqueCO2ListView;
+      
+
+        private EditText topicEditText;
 
         private Button retour;
         //private Button retourButton;
@@ -30,7 +32,7 @@ namespace MQTT
         private Button subscribeButton;
         private TextView messageTextView;
 
-        private Button subscribeGazButton;
+        private Button topicButton;
         private TextView messageGazTextView;
 
         private IMqttClient mqttClient;
@@ -43,8 +45,7 @@ namespace MQTT
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.content_main);
 
-            // Get ListView from layout
-            _historiqueCO2ListView = FindViewById<ListView>(Resource.Id.historiqueCO2ListView);
+            
 
             // Call the method to bind data to ListView
             //BindHistoriqueCO2Async();
@@ -53,8 +54,8 @@ namespace MQTT
             subscribeButton = FindViewById<Button>(Resource.Id.subscribeButton);
             messageTextView = FindViewById<TextView>(Resource.Id.messageTextView);
 
-            subscribeGazButton = FindViewById<Button>(Resource.Id.subscribeGazButton);
-            messageGazTextView = FindViewById<TextView>(Resource.Id.messageGazTextView);
+            topicButton = FindViewById<Button>(Resource.Id.inputButton2);
+            topicEditText = FindViewById<EditText>(Resource.Id.topic);
 
             retour = FindViewById<Button>(Resource.Id.retour);
 
@@ -67,7 +68,7 @@ namespace MQTT
 
             // Setup button click event handler
             subscribeButton.Click += OnSubscribeButtonClick;
-            subscribeGazButton.Click += OnSubscribeGazButtonClick;
+            topicButton.Click += OnTopicButtonClick;
 
             retour.Click += OnRetourButtonClick;
 
@@ -84,30 +85,6 @@ namespace MQTT
             StartActivity(intent);
         }
 
-
-        //private void OnRetourButtonClick(object sender, EventArgs e)
-        //{
-        //    // Create intent to redirect to OptionsActivity
-        //    // Launch the Dashboard activity if login is successful
-        //    Intent intent = new Intent(this, typeof(Options));
-        //    StartActivity(intent);
-        //}
-
-        //private async void BindHistoriqueCO2Async()
-        //{
-        //    var api = new ApiCall();
-        //    var historiqueCO2 = await api.GethistoriqueCO2Async();
-
-        //    // Create list of string to bind data to ListView
-        //    var historiqueCO2List = new List<string>();
-        //    foreach (var item in historiqueCO2)
-        //    {
-        //        historiqueCO2List.Add($"{item.Id} - Niveau: {item.Niveau}, Date: {item.Date}, UtilisateurId: {item.UtilisateurId}");
-        //    }
-
-        //    // Bind data to ListView using ArrayAdapter
-        //    _historiqueCO2ListView.Adapter = new ArrayAdapter<string>(this, Android.Resource.Layout.SimpleListItem1, historiqueCO2List);
-        //}
 
         private async void OnSubscribeButtonClick(object sender, EventArgs e)
         {
@@ -158,8 +135,12 @@ namespace MQTT
                 messageTextView.Text = "";
             }
         }
-        private async void OnSubscribeGazButtonClick(object sender, EventArgs e)
+
+        private async void OnTopicButtonClick(object sender, EventArgs e)
         {
+
+            string topic = topicEditText.Text;
+
             if (!isConnected)
             {
                 // Setup MQTT connection options
@@ -174,18 +155,17 @@ namespace MQTT
                 isConnected = true;
 
                 // Subscribe to GAZ topic
-                var subscribeResult = await mqttClient.SubscribeAsync(new TopicFilterBuilder().WithTopic("GAZ").Build());
+                var subscribeResult = await mqttClient.SubscribeAsync(new TopicFilterBuilder().WithTopic(topic).Build());
 
                 // Setup message received event handler
                 mqttClient.UseApplicationMessageReceivedHandler(async e =>
                 {
-                    // Handle GAZ topic
-                    if (e.ApplicationMessage.Topic == "GAZ")
-                    {
-                        // Display message in TextView
-                        messageGazTextView.Text = Encoding.UTF8.GetString(e.ApplicationMessage.Payload);
-                    }
-                    // Handle other topics here if needed
+                    string payload = Encoding.UTF8.GetString(e.ApplicationMessage.Payload);
+                    // Display message in TextView
+                    messageTextView.Text = e.ApplicationMessage.Topic + ": " + Encoding.UTF8.GetString(e.ApplicationMessage.Payload);
+                    AppData.MQTTService.MessageTopic = Encoding.UTF8.GetString(e.ApplicationMessage.Payload);
+                    AppData.MQTTService.TitreTopic = e.ApplicationMessage.Topic;
+
                 });
             }
             else
@@ -196,7 +176,7 @@ namespace MQTT
                 isConnected = false;
 
 
-                messageGazTextView.Text = "";
+               
             }
         }
 
